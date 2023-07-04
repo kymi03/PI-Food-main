7c364671d74cadb4aa366327ab44742009118635
kymi03 <kimysabela03@gmail.com>
2023-07-03 22:05:33 -0500

    deploy

--- PI-Food-main/api/index.js
+++ PI-Food-main/api/index.js
@@ -23,7 +23,7 @@ const port = process.env.PORT || 3001;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(port, () => {
+  server.listen(port, "0.0.0.0", () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
